ae2521ebbd5759da619cf1548f362d89deb1ef43
TsubasaMatsuura <tsubasa2832001@gmail.com>
2020-04-28 14:07:10 +0900

    labelテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_28_022649) do
+ActiveRecord::Schema.define(version: 2020_04_28_050635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "labels", force: :cascade do |t|
+    t.string "title"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_labels_on_user_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
@@ -34,4 +42,5 @@ ActiveRecord::Schema.define(version: 2020_04_28_022649) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "labels", "users"
 end
