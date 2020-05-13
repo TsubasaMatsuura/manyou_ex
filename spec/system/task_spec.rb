63f2dacb381c2a27a8769cc5966aaf786b34c26a
TsubasaMatsuura <tsubasa2832001@gmail.com>
2020-05-13 14:23:24 +0900

    taskspecにuserを追加

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -2,14 +2,24 @@ require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
   before do
 
-     FactoryBot.create(:task, name: '付け加えた名前1', detail: '付け加えたコメント1', progress: '完了')
-    @task2 = FactoryBot.create(:second_task, name: '付け加えた名前3', detail: '付け加えたコメント3')
+    @user = FactoryBot.create(:user)
+
+     FactoryBot.create(:task, name: '付け加えた名前1', detail: '付け加えたコメント1', progress: '完了', user: @user)
+    @task2 = FactoryBot.create(:second_task, name: '付け加えた名前3', detail: '付け加えたコメント3', user:@user)
+  end
+
+  def login
+    visit new_session_path
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: "00000000"
+    click_on "Log in"
   end
 
   describe 'タスク一覧画面' do
     context 'タスクを作成した場合' do
       it '作成済みのタスクが表示されること' do
         # beforeに必要なタスクデータが作成されるので、ここでテストデータ作成処理を書く必要がない
+        login
         visit tasks_path
         expect(page).to have_content '付け加えた名前1'
       end
@@ -17,6 +27,7 @@ RSpec.describe 'タスク管理機能', type: :system do
 
     context '複数のタスクを作成した場合' do
       it 'タスクが作成日時の降順に並んでいること' do
+        login
         visit tasks_path
         task_list = all('.task_row') # タスク一覧を配列として取得するため、View側でidを振っておく
 
@@ -29,6 +40,7 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe 'タスク登録画面' do
     context '必要項目を入力して、createボタンを押した場合' do
       it 'データが保存されること' do
+        login
         visit new_task_path
         fill_in "task_name", with: "abcdef"
         fill_in "task_detail", with: "ghijkl"
@@ -42,6 +54,7 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe 'タスク詳細画面' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示されたページに遷移すること' do
+        login
         task = FactoryBot.create(:task, name: 'wwwww', detail: 'xxxx')
         visit task_path(task)
          
@@ -54,7 +67,7 @@ RSpec.describe 'タスク管理機能', type: :system do
     context '最終期限ボタンを押したら' do
       
       before do
-        
+        login
         # 1 全てのタスクを取得してdeadlineを一日置きになる様に設定する
         all_tasks = Task.all
         all_tasks.each_with_index do |task, i|
@@ -63,6 +76,7 @@ RSpec.describe 'タスク管理機能', type: :system do
         end
        end
       it '期日が迫っている日から見せ、progressが登録されている' do    
+        login
         visit tasks_path
         click_on "終了期限でソート"
         
@@ -77,6 +91,7 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe 'タスク一覧' do
     context '進捗状態で完了、を指定後、検索を押したら' do
     it '完了タスクだけ表示' do
+        login
         visit tasks_path
         all_tasks = Task.all
         
@@ -95,6 +110,7 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe 'タスク一覧' do
     context 'タスク名Name検索で、付け加えた名前1、を指定後、検索を押したら' do
     it 'タスク名、付け加えた名前1、だけを表示' do
+        login
         visit tasks_path
         all_tasks = Task.all
         
